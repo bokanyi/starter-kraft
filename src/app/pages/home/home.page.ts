64f0b0e27fa81d377d18572734c4bbb9f740299b
bokanyi <bokanyimi@gmail.com>
2023-05-31 20:27:47 +0200

    tel email href added

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -16,7 +16,7 @@ export class HomePage implements OnInit {
     'Helyszíni felmérést, műszaki helyzetelemzést és az ingatlan építészeti lehetőségeinek feltárását, építészeti tanácsadást vállalunk + építészeti tervek elkészítését';
   name: string = 'Vizi Kata Veronika';
   email: string = 'kataveronikavizi@gmail.com';
-  tel: string = '06 30 843 07 22';
+  tel: string = '+36 30 843 07 22';
   instagram: string = 'https://www.instagram.com/archicare_hu/';
   address: string = `Ráday utca 31/B \nKapucsengő/Buzz: 164 - Közben Stúdió \nBudapest IX. kerület 1092`;
   targets: string =
